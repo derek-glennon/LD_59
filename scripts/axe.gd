1302e473384bce00bb6bd19f36400cc22c5eab1a
derek-glennon <derek.r.glennon@gmail.com>
2026-04-19 14:49:00 -0500

    Adding logs and log circle
    now to make fire

--- scripts/axe.gd
+++ scripts/axe.gd
@@ -1,5 +1,9 @@
 class_name Axe extends InteractableBase
 
+@export var log_scene : PackedScene
+@export var log_give_direction := Vector3.ZERO
+@export var log_give_force := 100.0
+
 func _physics_process(delta: float) -> void:
 	var collision_info = move_and_collide(Vector3.ZERO, true)
 	if collision_info:
@@ -7,4 +11,9 @@ func _physics_process(delta: float) -> void:
 		if tree:
 			if linear_velocity.length_squared() > 10.0:
 				tree.shake_tree()
-				# TODO : Spawn Log
+				var new_log := log_scene.instantiate() as Log
+				get_tree().root.add_child(new_log)
+				new_log.global_position = collision_info.get_position()
+				var force_direction = (collision_info.get_normal() + log_give_direction).normalized()
+				var force = force_direction * log_give_force
+				new_log.apply_force(force)
---
+++ scripts/log_circle.gd
@@ -0,0 +1,18 @@
+class_name LogCircle extends Node3D
+
+@export var logs : Array[MeshInstance3D] = []
+
+var _number_of_logs := 0
+
+func _on_log_snap_area_body_entered(body: Node3D) -> void:
+	var log = body as Log
+	if log:
+		logs[_number_of_logs].visible = true
+		_number_of_logs += 1
+		if _number_of_logs == logs.size():
+			start_fire()
+		log.queue_free()
+		
+func start_fire() -> void:
+	prints("FIRE")
+		
